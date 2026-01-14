8ddac7a1f52c2f334186d8a8f15689d2f053c26f
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2026-01-14 10:37:09 -0400

    Second Commit

---
+++ GeorgiaDavid_NewHealthSystem/Health.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgiaDavid_NewHealthSystem
+{
+    class Health
+    {
+       int _currentHealth;
+       int _maxHealth;
+
+        public Health(int maxHealth)
+        {
+            _currentHealth = maxHealth;
+            _maxHealth = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+            {
+                Console.WriteLine("Invalid damage");
+            }
+            else
+            {
+                _currentHealth -= damage;
+
+                _currentHealth = Math.Max(0, _currentHealth);
+            }
+        }
+
+        public void Restore()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        public void Heal(int health)
+        {
+            if(health < 0)
+            {
+                Console.WriteLine("Invalid health");
+            }
+            else
+            {
+                _currentHealth += health;
+
+                _currentHealth = Math.Min(_maxHealth, _currentHealth);
+            }
+        }
+    }
+}
---
+++ GeorgiaDavid_NewHealthSystem/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgiaDavid_NewHealthSystem
+{
+    class Player
+    {
+        string _name;
+
+
+        public void TakeDamage(int damage)
+        {
+            
+        }
+
+        public void GetStatusString()
+        {
+
+        }
+    }
+}
